d7db60a7d83cd828633c1707e5efe470f087db12
liuyunan <liuyunan.20@gmail.com>
2022-08-17 10:27:55 -0700

    There is a class named Circle. This class has one field radiusof the double type.
    Create two instance methods for this class:
    
    getLength that returns the double representing the length of the circumference of this circle;
    getArea that returns the double representing the area of this circle.
    In your calculations, you may use the special constant Math.PI.
    
    Do not make the field and methods private.

---
+++ Topics/Instance methods/Circle/src/Main.java
@@ -0,0 +1,12 @@
+class Circle {
+
+    double radius;
+
+    public double getLength() {
+        return 2 * radius * Math.PI;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
